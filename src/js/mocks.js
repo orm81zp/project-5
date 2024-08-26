f2990f3c0d4b9d62f74a975e2f59ffdab1055621
Roman Onishchenko <roman.onishchenko@onelog.ch>
2024-08-27 00:00:29 +0300

    add init commit

---
+++ src/js/mocks.js
@@ -0,0 +1,206 @@
+export const MOCK_FILTERS_RESPONSE = {
+  page: '1',
+  perPage: '9',
+  totalPages: 2,
+  results: [
+    {
+      filter: 'Body parts',
+      name: 'lower legs',
+      imgURL:
+        'https://ftp.goit.study/img/power-pulse/filters/lower_legs_thafch.jpg',
+    },
+    {
+      filter: 'Body parts',
+      name: 'back',
+      imgURL: 'https://ftp.goit.study/img/power-pulse/filters/back_wzzphw.jpg',
+    },
+    {
+      filter: 'Body parts',
+      name: 'shoulders',
+      imgURL:
+        'https://ftp.goit.study/img/power-pulse/filters/shoulders_deqz9d.jpg',
+    },
+    {
+      filter: 'Body parts',
+      name: 'waist',
+      imgURL: 'https://ftp.goit.study/img/power-pulse/filters/waist_i7kqzq.jpg',
+    },
+    {
+      filter: 'Body parts',
+      name: 'cardio',
+      imgURL:
+        'https://ftp.goit.study/img/power-pulse/filters/cardio_pkkceg.jpg',
+    },
+    {
+      filter: 'Body parts',
+      name: 'upper arms',
+      imgURL:
+        'https://ftp.goit.study/img/power-pulse/filters/upper_arms_xsndlt.jpg',
+    },
+    {
+      filter: 'Body parts',
+      name: 'neck',
+      imgURL: 'https://ftp.goit.study/img/power-pulse/filters/neck_t0fc1k.jpg',
+    },
+    {
+      filter: 'Body parts',
+      name: 'lower arms',
+      imgURL:
+        'https://ftp.goit.study/img/power-pulse/filters/lower_arms_hvwarx.jpg',
+    },
+    {
+      filter: 'Body parts',
+      name: 'upper legs',
+      imgURL:
+        'https://ftp.goit.study/img/power-pulse/filters/upper_legs_dlkfwx.jpg',
+    },
+  ],
+};
+
+export const MOCK_EXERCISES_BODYPART_RESPONSE = {
+  page: 1,
+  perPage: 10,
+  totalPages: 17,
+  results: [
+    {
+      _id: '64f389465ae26083f39b17a2',
+      bodyPart: 'waist',
+      equipment: 'body weight',
+      gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0001.gif',
+      name: '3/4 sit-up',
+      target: 'abs',
+      description:
+        "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+      rating: 3.92,
+      burnedCalories: 220,
+      time: 3,
+      popularity: 16782,
+    },
+    {
+      _id: '64f389465ae26083f39b17a3',
+      bodyPart: 'waist',
+      equipment: 'body weight',
+      gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0002.gif',
+      name: '45° side bend',
+      target: 'abs',
+      description:
+        "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+      rating: 4.19,
+      burnedCalories: 323,
+      time: 3,
+      popularity: 1828,
+    },
+    {
+      _id: '64f389465ae26083f39b17a4',
+      bodyPart: 'waist',
+      equipment: 'body weight',
+      gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0003.gif',
+      name: 'air bike',
+      target: 'abs',
+      description:
+        "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+      rating: 4.42,
+      burnedCalories: 312,
+      time: 3,
+      popularity: 3354,
+    },
+    {
+      _id: '64f389465ae26083f39b17a5',
+      bodyPart: 'waist',
+      equipment: 'body weight',
+      gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0006.gif',
+      name: 'alternate heel touchers',
+      target: 'abs',
+      description:
+        "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+      rating: 3.79,
+      burnedCalories: 116,
+      time: 3,
+      popularity: 9165,
+    },
+    {
+      _id: '64f389465ae26083f39b17ac',
+      bodyPart: 'waist',
+      equipment: 'medicine ball',
+      gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0014.gif',
+      name: 'assisted motion russian twist',
+      target: 'abs',
+      description:
+        "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+      rating: 3.57,
+      burnedCalories: 212,
+      time: 3,
+      popularity: 651,
+    },
+    {
+      _id: '64f389465ae26083f39b1935',
+      bodyPart: 'waist',
+      equipment: 'body weight',
+      gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0464.gif',
+      name: 'front plank with twist',
+      target: 'abs',
+      description:
+        "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+      rating: 4.23,
+      burnedCalories: 174,
+      time: 3,
+      popularity: 413,
+    },
+    {
+      _id: '64f389465ae26083f39b1937',
+      bodyPart: 'waist',
+      equipment: 'body weight',
+      gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0467.gif',
+      name: 'gorilla chin',
+      target: 'abs',
+      description:
+        "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+      rating: 3.8,
+      burnedCalories: 105,
+      time: 3,
+      popularity: 282,
+    },
+    {
+      _id: '64f389465ae26083f39b1947',
+      bodyPart: 'waist',
+      equipment: 'body weight',
+      gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0495.gif',
+      name: 'incline twisting sit-up',
+      target: 'abs',
+      description:
+        "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+      rating: 3.17,
+      burnedCalories: 229,
+      time: 3,
+      popularity: 374,
+    },
+    {
+      _id: '64f389465ae26083f39b194f',
+      bodyPart: 'waist',
+      equipment: 'body weight',
+      gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0508.gif',
+      name: 'janda sit-up',
+      target: 'abs',
+      description:
+        "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+      rating: 3.33,
+      burnedCalories: 172,
+      time: 3,
+      popularity: 236,
+    },
+    {
+      _id: '64f389465ae26083f39b17e3',
+      bodyPart: 'waist',
+      equipment: 'barbell',
+      gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0071.gif',
+      name: 'barbell press sit-up',
+      target: 'abs',
+      description:
+        "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+      rating: 3.4,
+      burnedCalories: 156,
+      time: 3,
+      popularity: 307,
+    },
+  ],
+};
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import './js/mobileMenu';
+import './js/modal';
