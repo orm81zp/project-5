b61e24fdb2c2cae43d1bbd7080ab4a0c062db0ab
sergio <sergii.kapustin@gmail.com>
2024-08-28 21:18:49 +0300

    scroll-up feature

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import './js/mobileMenu';
 import './js/modal';
+import { calcScrollValue } from './js/scroll-up';
+
+const scrollProgress = document.querySelector('.scroll-up');
+
+scrollProgress.addEventListener('click', () => {
+  document.documentElement.scrollTop = 0;
+});
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
