fc14d74be773bf95acf3a2ce74d192aaf18a3f22
DanSport <bogdan.onoyko@gmail.com>
2024-08-28 13:00:03 +0200

    js footer added

---
+++ src/js/footer.js
@@ -0,0 +1,88 @@
+import Notiflix from 'notiflix';
+import Api from './api/index';
+import { gsap } from 'gsap';
+
+const formSubmit = document.querySelector('.js-footer-form');
+const btnSubmit = document.querySelector('.footer_form_btn');
+formSubmit.addEventListener('submit', fetchSubscription);
+
+function isValidEmail(email) {
+  // const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const emailPattern = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+  return emailPattern.test(email);
+}
+
+btnSubmit.disabled = true;
+
+emailInput.addEventListener('input', () => {
+  const email = emailInput.value;
+
+  if (isValidEmail(email)) {
+    btnSubmit.style.backgroundColor = '#c6cdd1';
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.style.backgroundColor = '#f4f4f4';
+    btnSubmit.disabled = true;
+  }
+});
+
+function fetchSubscription(event) {
+  event.preventDefault();
+
+  const emailInput = document.querySelector('input[type="email"]');
+  const email = emailInput.value;
+
+  if (!isValidEmail(email)) {
+    return Notiflix.Notify.failure('Invalid email address was entered.');
+  }
+
+  const subscriptionData = {
+    email: email,
+  };
+
+  Api.addSubscription(subscriptionData)
+    .then(resp => {
+      const massage = resp.data.message;
+      Notiflix.Notify.success(massage);
+    })
+    .catch(error => {
+      const badRequest = error.response.data.message;
+      if (error.response.status === 409) {
+        Notiflix.Notify.warning('Subscription already exists');
+      }
+      if (error.response.status === 400) {
+        Notiflix.Notify.warning(badRequest);
+      }
+    });
+  formSubmit.reset();
+}
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0,
+};
+const icon = document.querySelector('.footer_logo_icon');
+const text = document.querySelector('.footer_title_span');
+
+const observer = new IntersectionObserver(handleIntersection, options);
+
+observer.observe(icon);
+
+function handleIntersection(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    gsap.to(icon, {
+      duration: 2,
+      opacity: 1,
+      x: 0,
+      rotationX: 360,
+    });
+    gsap.to(text, {
+      duration: 2,
+      opacity: 1,
+    });
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './js/favorites';
 import './js/mobileMenu';
 import './js/modal';
+import './js/footer';
