6511aa648ecaabd5328efeffe703b83d753b4e77
oleg-usatyi <oleg_usatyi@ukr.net>
2024-08-28 20:21:44 +0200

    header - minor fix

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,1 +1,30 @@
 // mobile menu implementation
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container')
+  const openMenuBtn = document.querySelector('.js-open-menu')
+  const closeMenuBtn = document.querySelector('.js-close-menu')
+  const menuLinks = document.querySelectorAll('.menu-link')
+  const menuOverlay = document.querySelector('.mobile-menu-overlay')
+
+  const toggleMenu = () => {
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen)
+    mobileMenu.classList.toggle('is-open')
+  }
+
+  openMenuBtn.addEventListener('click', toggleMenu)
+  closeMenuBtn.addEventListener('click', toggleMenu)
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', toggleMenu)
+  })
+  menuOverlay.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) {
+      return
+    }
+    mobileMenu.classList.remove('is-open')
+    openMenuBtn.setAttribute('aria-expanded', false)
+  });
+})();
