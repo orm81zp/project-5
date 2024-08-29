94d8101b6731c7e6e45166ec38db35d52800c337
Antonina Sych <antoninasych@gmail.com>
2024-08-29 21:50:04 +0100

    add sidebar and a quote of the day (#8)

---
+++ src/js/quote.js
@@ -0,0 +1,29 @@
+import Api from './api';
+
+async function fetchAndSaveQuote() {
+  try{
+  const today = new Date().toLocaleDateString();
+  const savedQuoteData = JSON.parse(localStorage.getItem('quoteData'));
+  if (savedQuoteData && savedQuoteData.date === today) {
+    return savedQuoteData.quote;
+  }
+
+    let quote = await Api.getQuote();
+
+  const newQuoteData = {
+    date: today,
+    quote: quote
+  };
+  localStorage.setItem('quoteData', JSON.stringify(newQuoteData));
+
+  return newQuoteData.quote;}catch (e){
+    console.log('Error during  getting quote')
+  }
+}
+
+
+fetchAndSaveQuote().then(quote => {
+  document.getElementById('quote_text').innerText = quote.quote;
+  document.getElementById('quote_author').innerText = quote.author;
+
+});
