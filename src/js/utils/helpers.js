1360eebd9136d76dbd873370d155371c3320bfad
Roman Onishchenko <roman.onishchenko@onelog.ch>
2024-08-31 16:34:58 +0300

    add empty row at the end of a file

--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -26,7 +26,6 @@ export const clearContent = containers => {
 };
 
 export function isValidEmail(email) {
-  const emailPattern =
-    /^\w+(.\w+)?@[a-zA-Z_]+?.[a-zA-Z]{2,3}$/;
+  const emailPattern = /^\w+(.\w+)?@[a-zA-Z_]+?.[a-zA-Z]{2,3}$/;
   return emailPattern.test(email);
 }
